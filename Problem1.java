bf6b787e1cd3fd27dc67c95a2f94d3a5cd3ff1ea
Santosh Kallala <kallalasantosh@gmail.com>
2020-07-13 19:27:56 -0400

    problem1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,31 @@
-
+public class Problem1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6, 7, 8};
+        int sizeArr =8;
+        System.out.println(missingEllement(arr,sizeArr));
+    }
+    public static int missingEllement(int[] arr,int size){
+        int start =0;
+        int end = arr.length-1;
+        int mid =-1;
+        if(size==end || arr==null||arr.length==0){
+            return -1;
+        }
+        while(end-start>=1){
+            mid = start +(end-start)/2;
+            if(end-start==1){
+                if(arr[mid]==mid+2){
+                    return mid+1;
+                }else{
+                    return arr[mid]+1;
+                }
+            }
+            if(arr[mid]>mid+1){
+                end = mid;
+            }else{
+                start =mid;
+            }
+        }
+        return -1;
+    }
+}
